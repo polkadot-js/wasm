8e5c46e964ed910604be6ab28fdb63e7e9e0a5e9
Jaco Greeff <jacogr@gmail.com>
2019-03-15 13:18:15 +0100

    Travis matrix for JS & Rust

---
+++ test-js.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# test packages
+echo "*** Testing JS packages"
+cd packages
+cd wasm-schnorrkel
+cd ..
+cd ..
---
+++ test-rust.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# test packages
+echo "*** Testing Rust packages"
+cd packages
+cd wasm-schnorrkel
+cd ..
+cd ..
