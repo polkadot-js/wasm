f19334c1ab3eb8ecf2b1205a27e682d3b5e153bc
Jaco Greeff <jacogr@gmail.com>
2019-03-17 17:41:30 +0100

    Headers

--- packages/wasm-crypto/test/all.js
+++ packages/wasm-crypto/test/all.js
@@ -1,9 +1,9 @@
-// Copyright 2019 @polkadot/wasm-schnorrkel authors & contributors
+// Copyright 2019 @polkadot/wasm-crypto authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 // @ts-check
 
-const { assert, hexToU8a, stringToU8a, u8aToHex } = require('@polkadot/util');
+const { assert, stringToU8a, u8aToHex } = require('@polkadot/util');
 const crypto = require('../build/index');
 
 async function beforeAll () {
