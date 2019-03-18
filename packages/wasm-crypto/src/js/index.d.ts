b281adec8b98894effdcfe533fe8a3966f7a1549
Jaco Greeff <jacogr@gmail.com>
2019-03-18 10:29:06 +0100

    Adjust signatures

--- packages/wasm-crypto/src/js/index.d.ts
+++ packages/wasm-crypto/src/js/index.d.ts
@@ -2,10 +2,10 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export function blake2bHash (data: Uint8Array, key: Uint8Array, size: number): Uint8Array;
+export function blake2bHash (data: Uint8Array, key: Uint8Array, byteSize: number): Uint8Array;
 export function pbkdf2Hash (data: Uint8Array, salt: Uint8Array, rounds: number): Uint8Array;
 export function sha512Hash (data: Uint8Array): Uint8Array;
-export function twoxHash (data: Uint8Array, seed: number): Uint8Array;
+export function twoxHash (data: Uint8Array, rounds: number): Uint8Array;
 
 export function isReady (): boolean;
 export function waitReady (): Promise<boolean>;
--- packages/wasm-schnorrkel/test/all.js
+++ packages/wasm-schnorrkel/test/all.js
@@ -8,19 +8,11 @@ const { assert, hexToU8a, stringToU8a, u8aToHex } = require('@polkadot/util');
 const schnorrkel = require('../build/index');
 
 function extractKeys (pair) {
-  const pk = pair.slice(64);
-  const sk = pair.slice(0, 64);
-
-  return [pair, pk, sk];
+  return [pair, pair.slice(64), pair.slice(0, 64)];
 }
 
 function randomPair () {
-  const seed = crypto.randomBytes(32);
-  const pair = schnorrkel.keypairFromSeed(seed);
-
-  assert(pair.length === 96, 'ERROR: Invalid pair created');
-
-  return extractKeys(pair);
+  return extractKeys(schnorrkel.keypairFromSeed(crypto.randomBytes(32)));
 }
 
 async function beforeAll () {
@@ -29,12 +21,7 @@ async function beforeAll () {
 
 function pairFromSeed () {
   const SEED = stringToU8a('12345678901234567890123456789012');
-  const PAIR = hexToU8a(
-    '0x' +
-    'f0106660c3dda23f16daa9ac5b811b963077f5bc0af89f85804f0de8e424f050' +
-    'f98d66f39442506ff947fd911f18c7a7a5da639a63e8d3b4e233f74143d951c1' +
-    '741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63'
-  );
+  const PAIR = hexToU8a('0xf0106660c3dda23f16daa9ac5b811b963077f5bc0af89f85804f0de8e424f050f98d66f39442506ff947fd911f18c7a7a5da639a63e8d3b4e233f74143d951c1741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63');
 
   const pair = schnorrkel.keypairFromSeed(SEED);
 
@@ -46,12 +33,7 @@ function pairFromSeed () {
 
 function devFromSeed () {
   const SEED = hexToU8a('0xfac7959dbfe72f052e5a0c3c8d6530f202b02fd8f9f5ca3580ec8deb7797479e');
-  const PAIR = hexToU8a(
-    '0x' +
-    '28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51' +
-    'fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca34' +
-    '46ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a'
-  );
+  const PAIR = hexToU8a('0x28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca3446ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a');
 
   const pair = schnorrkel.keypairFromSeed(SEED);
 
@@ -64,11 +46,8 @@ function devFromSeed () {
 function verifyExisting () {
   const PK = hexToU8a('0x741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63');
   const MESSAGE = stringToU8a('this is a message');
-  const SIGNATURE = hexToU8a(
-    '0x' +
-    'decef12cf20443e7c7a9d406c237e90bcfcf145860722622f92ebfd5eb4b5b39' +
-    '90b6443934b5cba8f925a0ae75b3a77d35b8490cbb358dd850806e58eaf72904'
-  );
+  const SIGNATURE = hexToU8a('0xdecef12cf20443e7c7a9d406c237e90bcfcf145860722622f92ebfd5eb4b5b3990b6443934b5cba8f925a0ae75b3a77d35b8490cbb358dd850806e58eaf72904'
+);
 
   const isValid = schnorrkel.verify(SIGNATURE, MESSAGE, PK);
 
@@ -101,12 +80,7 @@ function deriveHard () {
 
 function deriveHardKnown () {
   const CC = hexToU8a('0x14416c6963650000000000000000000000000000000000000000000000000000');
-  const PAIR = hexToU8a(
-    '0x' +
-    '28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51' +
-    'fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca34' +
-    '46ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a'
-  );
+  const PAIR = hexToU8a('0x28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca3446ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a');
   const PUBLIC = '0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d';
 
   const derived = schnorrkel.deriveKeypairHard(PAIR, CC);
@@ -160,10 +134,7 @@ function benchmark () {
   for (let i = 0; i < 256; i++) {
     const [, pk, sk] = randomPair();
 
-    const signature = schnorrkel.sign(pk, sk, MESSAGE);
-    const isValid = schnorrkel.verify(signature, MESSAGE, pk);
-
-    assert(isValid, 'ERROR: Unable to verify signature');
+    assert(schnorrkel.verify(schnorrkel.sign(pk, sk, MESSAGE), MESSAGE, pk), 'ERROR: Unable to verify signature');
   }
 }
 
