0f14477b75841c45eb1901ba74c22951da07be39
Jaco Greeff <jacogr@gmail.com>
2019-03-22 08:45:47 +0100

    Update ed25519 tests

--- packages/wasm-dalek-ed25519/src/lib.rs
+++ packages/wasm-dalek-ed25519/src/lib.rs
@@ -148,22 +148,20 @@ pub mod tests {
 
 	#[test]
 	fn can_verify_known() {
-		let pair = keypair_from_seed(b"12345678901234567890123456789012");
-		let public = &pair[32..];
-		let message = &[0x61, 0x62, 0x63, 0x64];
-		let signature = &[28, 58, 206, 239, 249, 70, 59, 191, 166, 40, 219, 218, 235, 170, 25, 79, 10, 94, 9, 197, 34, 126, 1, 150, 246, 68, 28, 238, 36, 26, 172, 163, 168, 90, 202, 211, 126, 246, 57, 212, 43, 24, 88, 197, 240, 113, 118, 76, 37, 81, 91, 110, 236, 50, 144, 134, 100, 223, 220, 238, 34, 185, 211, 7];
-		let is_valid = verify(signature, message, public);
+		let public = hex!("2f8c6129d816cf51c374bc7f08c3e63ed156cf78aefb4a6550d97b87997977ee");
+		let message = b"this is a message";
+		let signature = hex!("90588f3f512496f2dd40571d162e8182860081c74e2085316e7c4396918f07da412ee029978e4dd714057fe973bd9e7d645148bf7b66680d67c93227cde95202");
+		let is_valid = verify(&signature, message, &public);
 
 		assert!(is_valid);
 	}
 
 	#[test]
 	fn can_verify_known_wrong() {
-		let pair = keypair_from_seed(b"12345678901234567890123456789012");
-		let public = &pair[32..];
-		let message = &[0x61, 0x62, 0x63, 0x64, 0x99];
-		let signature = &[28, 58, 206, 239, 249, 70, 59, 191, 166, 40, 219, 218, 235, 170, 25, 79, 10, 94, 9, 197, 34, 126, 1, 150, 246, 68, 28, 238, 36, 26, 172, 163, 168, 90, 202, 211, 126, 246, 57, 212, 43, 24, 88, 197, 240, 113, 118, 76, 37, 81, 91, 110, 236, 50, 144, 134, 100, 223, 220, 238, 34, 185, 211, 7];
-		let is_valid = verify(signature, message, public);
+		let public = hex!("2f8c6129d816cf51c374bc7f08c3e63ed156cf78aefb4a6550d97b87997977ee");
+		let message = b"this is a message";
+		let signature = &[0u8; 64];
+		let is_valid = verify(signature, message, &public);
 
 		assert_eq!(is_valid, false);
 	}
--- packages/wasm-dalek-ed25519/test/all.js
+++ packages/wasm-dalek-ed25519/test/all.js
@@ -16,34 +16,12 @@ function randomPair () {
 }
 
 function pairFromSeed () {
-  const pair = dalek.keypairFromSeed(stringToU8a('12345678901234567890123456789012'));
+  const [pair, pk, sk] = extractKeys(dalek.keypairFromSeed(stringToU8a('12345678901234567890123456789012')));
 
-  console.log('\tSEC', u8aToHex(pair.slice(0, 64)));
-  console.log('\tPUB', u8aToHex(pair.slice(64)));
+  console.log('\tSEC', u8aToHex(sk));
+  console.log('\tPUB', u8aToHex(pk));
 
-  assert(u8aToHex(pair) === '0xf0106660c3dda23f16daa9ac5b811b963077f5bc0af89f85804f0de8e424f050f98d66f39442506ff947fd911f18c7a7a5da639a63e8d3b4e233f74143d951c1741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63', 'ERROR: pairFromSeed() does not match');
-}
-
-function devFromSeed () {
-  const pair = dalek.keypairFromSeed(hexToU8a('0xfac7959dbfe72f052e5a0c3c8d6530f202b02fd8f9f5ca3580ec8deb7797479e'));
-
-  console.log('\tSEC', u8aToHex(pair.slice(0, 64)));
-  console.log('\tPUB', u8aToHex(pair.slice(64)));
-
-  assert(u8aToHex(pair) === '0x28b0ae221c6bb06856b287f60d7ea0d98552ea5a16db16956849aa371db3eb51fd190cce74df356432b410bd64682309d6dedb27c76845daf388557cbac3ca3446ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a', 'ERROR: devFromSeed() does not match');
-}
-
-function verifyExisting () {
-  const PK = hexToU8a('0x741c08a06f41c596608f6774259bd9043304adfa5d3eea62760bd9be97634d63');
-  const MESSAGE = stringToU8a('this is a message');
-  const SIGNATURE = hexToU8a('0xdecef12cf20443e7c7a9d406c237e90bcfcf145860722622f92ebfd5eb4b5b3990b6443934b5cba8f925a0ae75b3a77d35b8490cbb358dd850806e58eaf72904'
-);
-
-  const isValid = dalek.verify(SIGNATURE, MESSAGE, PK);
-
-  console.log('\tRES', isValid);
-
-  assert(isValid, 'ERROR: Unable to verify signature');
+  assert(u8aToHex(pair) === '0x31323334353637383930313233343536373839303132333435363738393031322f8c6129d816cf51c374bc7f08c3e63ed156cf78aefb4a6550d97b87997977ee', 'ERROR: pairFromSeed() does not match');
 }
 
 function signAndVerify () {
@@ -52,6 +30,14 @@ function signAndVerify () {
   const isValid = dalek.verify(signature, stringToU8a('this is a message'), pk);
 
   console.log('\tSIG', u8aToHex(signature));
+  console.log('\tRES', isValid);
+
+  assert(isValid, 'ERROR: Unable to verify signature');
+}
+
+function verifyExisting () {
+  const isValid = dalek.verify(hexToU8a('0x90588f3f512496f2dd40571d162e8182860081c74e2085316e7c4396918f07da412ee029978e4dd714057fe973bd9e7d645148bf7b66680d67c93227cde95202'), stringToU8a('this is a message'), hexToU8a('0x2f8c6129d816cf51c374bc7f08c3e63ed156cf78aefb4a6550d97b87997977ee'));
+
   console.log('\tRES', isValid);
 
   assert(isValid, 'ERROR: Unable to verify signature');
@@ -69,7 +55,6 @@ function benchmark () {
 
 const tests = {
   pairFromSeed,
-  devFromSeed,
   signAndVerify,
   verifyExisting,
   benchmark
