6f30202e87aaf406168c46773149ec7a001c795e
Jaco Greeff <jacogr@gmail.com>
2019-03-30 20:18:00 +0100

    Re-add drropped assert import

--- packages/wasm-crypto/src/js/index.js
+++ packages/wasm-crypto/src/js/index.js
@@ -2,6 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+const { assert } = require('@polkadot/util');
 const stubbed = require('./wasm');
 
 const INIT_ERRROR = '@polkadot/wasm-crypto has not been initialized';
