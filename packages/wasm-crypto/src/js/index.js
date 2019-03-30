659aed393fdc0da885dfe22e07003666771fbb6d
Jaco Greeff <jacogr@gmail.com>
2019-03-30 20:12:01 +0100

    Update error logging

--- packages/wasm-crypto/src/js/index.js
+++ packages/wasm-crypto/src/js/index.js
@@ -2,12 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const { assert } = require('@polkadot/util');
 const stubbed = require('./wasm');
+
+const INIT_ERRROR = '@polkadot/wasm-crypto has not been initialized';
 
 const wrapReady = (fn) =>
   (...params) => {
-    assert(stubbed.isReady(), 'ERROR: @polkadot/wasm-crypto has not been initialized');
+    assert(stubbed.isReady(), INIT_ERRROR);
 
     return fn(...params);
   };
