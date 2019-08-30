bd18b42439186379bbb14a2fa6624cb1478b0055
Jaco Greeff <jacogr@gmail.com>
2019-08-30 12:11:20 +0200

    typescript-eslint 2 (#33)
    * typescript-eslint 2
    
    * Blind eslint fixes

--- packages/wasm-crypto/test/all/index.js
+++ packages/wasm-crypto/test/all/index.js
@@ -2,9 +2,7 @@
 // Copyright 2019 @polkadot/wasm-crypto authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-// @ts-check
 
-// @ts-ignore
 const wasm = require('../../build/index');
 const bip39 = require('./bip39');
 const ed25519 = require('./ed25519');
@@ -18,7 +16,7 @@ const tests = {
   ...sr25519
 };
 
-async function beforeAll () {
+function beforeAll () {
   return wasm.waitReady();
 }
 
--- packages/wasm-crypto/test/jest.spec.js
+++ packages/wasm-crypto/test/jest.spec.js
@@ -2,16 +2,15 @@
 // Copyright 2019 @polkadot/wasm-crypto authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-// @ts-check
 
-// @ts-ignore
 require('../build/crypto-polyfill');
 
-// @ts-ignore
 const { beforeAll, tests, wasm } = require('./all');
 
 describe('schnorrkel-js', () => {
-  beforeEach(async () => beforeAll());
+  beforeEach(async () => {
+    await beforeAll();
+  });
 
   Object.keys(tests).forEach((name) => {
     it(name, () => tests[name](wasm));
