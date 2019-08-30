4dfd0cebc192631421599c17e342d1657e2fe86a
Jaco Greeff <jacogr@gmail.com>
2019-08-30 11:29:27 +0200

    typescript-eslint 2

--- packages/wasm-crypto/test/jest.spec.js
+++ packages/wasm-crypto/test/jest.spec.js
@@ -2,16 +2,13 @@
 // Copyright 2019 @polkadot/wasm-crypto authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-// @ts-check
 
-// @ts-ignore
 require('../build/crypto-polyfill');
 
-// @ts-ignore
 const { beforeAll, tests, wasm } = require('./all');
 
 describe('schnorrkel-js', () => {
-  beforeEach(async () => beforeAll());
+  beforeEach(() => beforeAll());
 
   Object.keys(tests).forEach((name) => {
     it(name, () => tests[name](wasm));
