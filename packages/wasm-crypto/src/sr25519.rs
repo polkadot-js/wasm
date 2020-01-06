e7d74be6c77f8da80380f015547128835cc050fa
Jaco Greeff <jacogr@gmail.com>
2020-01-06 13:25:54 +0100

    Bump deps

--- packages/wasm-crypto/src/sr25519.rs
+++ packages/wasm-crypto/src/sr25519.rs
@@ -1,5 +1,5 @@
 // Copyright 2019 Paritytech via https://github.com/paritytech/schnorrkel-js/
-// Copyright 2019 @polkadot/wasm-schnorrkel authors & contributors
+// Copyright 2019-2020 @polkadot/wasm-crypto authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
