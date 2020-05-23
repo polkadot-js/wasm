a4292512ebe0c224773b59aad5ca527d654598a8
Jaco Greeff <jacogr@gmail.com>
2020-05-23 12:30:40 +0200

    Bump deps

--- packages/wasm-crypto/test/all/bip39.js
+++ packages/wasm-crypto/test/all/bip39.js
@@ -3,7 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 // @ts-check
 
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable camelcase */
 
 const { assert, u8aToHex } = require('@polkadot/util');
 
--- packages/wasm-crypto/test/all/sr25519.js
+++ packages/wasm-crypto/test/all/sr25519.js
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable camelcase */
 
 const crypto = require('crypto');
 const { assert, hexToU8a, stringToU8a, u8aToHex } = require('@polkadot/util');
