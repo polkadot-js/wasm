1953a7fd3eebcb78e7223eec4b0981c226bbdf1c
Jaco Greeff <jacogr@gmail.com>
2020-11-02 12:11:48 +0100

    Remove exports

--- packages/wasm-crypto/src/js/index.js
+++ packages/wasm-crypto/src/js/index.js
@@ -1,8 +1,53 @@
 // Copyright 2019-2020 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable sort-keys */
+
 require('./crypto-polyfill');
+
+const { assert } = require('@polkadot/util');
 
 const stubbed = require('./wbindgen');
 
-module.exports = require('./exports')(stubbed);
+const INIT_ERROR = 'The WASM interface has not been initialized. Ensure that you wait for the initialization Promise with waitReady() from @polkadot/wasm-crypto (or cryptoWaitReady() from @polkadot/util-crypto) before attempting to use WASM-only interfaces.';
+
+function wrapReady (fn) {
+  return (...params) => {
+    assert(stubbed.isReady(), INIT_ERROR);
+
+    return fn(...params);
+  };
+}
+
+module.exports = {
+  bip39Generate: wrapReady(stubbed.ext_bip39_generate),
+  bip39ToEntropy: wrapReady(stubbed.ext_bip39_to_entropy),
+  bip39ToMiniSecret: wrapReady(stubbed.ext_bip39_to_mini_secret),
+  bip39ToSeed: wrapReady(stubbed.ext_bip39_to_seed),
+  bip39Validate: wrapReady(stubbed.ext_bip39_validate),
+
+  ed25519KeypairFromSeed: wrapReady(stubbed.ext_ed_from_seed),
+  ed25519Sign: wrapReady(stubbed.ext_ed_sign),
+  ed25519Verify: wrapReady(stubbed.ext_ed_verify),
+
+  sr25519DeriveKeypairHard: wrapReady(stubbed.ext_sr_derive_keypair_hard),
+  sr25519DeriveKeypairSoft: wrapReady(stubbed.ext_sr_derive_keypair_soft),
+  sr25519DerivePublicSoft: wrapReady(stubbed.ext_sr_derive_public_soft),
+  sr25519KeypairFromSeed: wrapReady(stubbed.ext_sr_from_seed),
+  sr25519Sign: wrapReady(stubbed.ext_sr_sign),
+  sr25519Verify: wrapReady(stubbed.ext_sr_verify),
+
+  blake2b: wrapReady(stubbed.ext_blake2b),
+  keccak256: wrapReady(stubbed.ext_keccak256),
+  pbkdf2: wrapReady(stubbed.ext_pbkdf2),
+  scrypt: wrapReady(stubbed.ext_scrypt),
+
+  // secp256k1IsRecoverable: wrapReady(stubbed.ext_secp256k1_is_recoverable);
+  // secp256k1Recover: wrapReady(stubbed.ext_secp256k1_recover);
+
+  sha512: wrapReady(stubbed.ext_sha512),
+  twox: wrapReady(stubbed.ext_twox),
+
+  isReady: stubbed.isReady,
+  waitReady: stubbed.waitReady
+};
