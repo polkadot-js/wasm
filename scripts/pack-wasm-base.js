b6588fde559b42473715e29c973fa6d7745f509f
Jaco Greeff <jacogr@gmail.com>
2020-12-12 12:41:40 +0100

    Allow fflate tree-shaking

--- scripts/pack-wasm-base.js
+++ scripts/pack-wasm-base.js
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const fs = require('fs');
-const lz4 = require('lz4js');
 const { formatNumber } = require('@polkadot/util');
 
 const A_NAME = 'bytes-asmjs-crypto';
@@ -16,11 +15,16 @@ function hdr (package) {
 `;
 }
 
-function getWasmBuffer () {
+// only one of the deps are included, here are the details for package.json -
+//   "fflate": "^0.4.2"
+//   "lz4js": "^0.2.0"
+function getWasmBuffer (lib = 'fflate') {
   const data = fs.readFileSync('./bytes/wasm_opt.wasm');
-  const comp = Buffer.from(lz4.compress(data));
+  const comp = lib === 'fflate'
+    ? Buffer.from(require('fflate').zlibSync(data, { level: 9 }))
+    : Buffer.from(require('lz4js').compress(data));
 
-  console.log(`*** Compressed WASM ${formatNumber(data.length)} -> ${formatNumber(comp.length)} (${(100 * comp.length / data.length).toFixed(2)}%)`);
+  console.log(`*** Compressed WASM via ${lib}: ${formatNumber(data.length)} -> ${formatNumber(comp.length)} (${(100 * comp.length / data.length).toFixed(2)}%)`);
 
   return comp.toString('base64');
 }
@@ -29,14 +33,12 @@ fs.writeFileSync(`./${A_NAME}/build/data.mjs`, `${hdr(A_NAME)}
 export { asmJsInit } from './data.js';
 `);
 
-fs.writeFileSync(`./${W_NAME}/build/data.mjs`, `${hdr(W_NAME)}
-export { wasmBytes } from './data.js';
+fs.writeFileSync(`./${W_NAME}/build/buffer.mjs`, `${hdr(W_NAME)}
+export { buffer } from './buffer.js';
 `);
 
-fs.writeFileSync('./bytes-wasm-crypto/build/data.js', `${hdr(W_NAME)}
-const lz4 = require('lz4js');
+fs.writeFileSync('./bytes-wasm-crypto/build/buffer.js', `${hdr(W_NAME)}
+const buffer = Buffer.from('${getWasmBuffer()}', 'base64');
 
-const wasmBytes = lz4.decompress(Buffer.from('${getWasmBuffer()}', 'base64'));
-
-module.exports = { wasmBytes };
+module.exports = { buffer };
 `);
