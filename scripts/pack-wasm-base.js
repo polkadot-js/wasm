3058229102231a9b4ac81beb014e68112c84fc09
Jaco Greeff <jacogr@gmail.com>
2020-12-12 11:45:11 +0100

    Use lz4 on the WASM stream

--- scripts/pack-wasm-base.js
+++ scripts/pack-wasm-base.js
@@ -2,15 +2,28 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const fs = require('fs');
+const lz4 = require('lz4js');
+const { formatNumber } = require('@polkadot/util');
 
 const A_NAME = 'bytes-asmjs-crypto';
 const W_NAME = 'bytes-wasm-crypto';
 
-const hdr = (package) => `// Copyright 2019-2020 @polkadot/${package} authors & contributors
+function hdr (package) {
+  return `// Copyright 2019-2020 @polkadot/${package} authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 // Generated as part of the build, do not edit
 `;
+}
+
+function getWasmBuffer () {
+  const data = fs.readFileSync('./bytes/wasm_opt.wasm');
+  const comp = Buffer.from(lz4.compress(data));
+
+  console.log(`*** Compressed WASM ${formatNumber(data.length)} -> ${formatNumber(comp.length)} (${(100 * comp.length / data.length).toFixed(2)}%)`);
+
+  return comp.toString('base64');
+}
 
 fs.writeFileSync(`./${A_NAME}/build/data.mjs`, `${hdr(A_NAME)}
 export { asmJsInit } from './data.js';
@@ -21,5 +34,9 @@ export { wasmBytes } from './data.js';
 `);
 
 fs.writeFileSync('./bytes-wasm-crypto/build/data.js', `${hdr(W_NAME)}
-module.exports = { wasmBytes: Buffer.from('${fs.readFileSync('./bytes/wasm_opt.wasm').toString('base64')}', 'base64') };
+const lz4 = require('lz4js');
+
+const wasmBytes = lz4.decompress(Buffer.from('${getWasmBuffer()}', 'base64'));
+
+module.exports = { wasmBytes };
 `);
