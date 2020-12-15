d63fe1107ef0d2678b993d4880899fbbb8b1c913
Jaco Greeff <jacogr@gmail.com>
2020-12-15 15:57:33 +0100

    Revert unneeded from-build alias

--- packages/wasm-crypto/test/alias-imports.js
+++ packages/wasm-crypto/test/alias-imports.js
@@ -4,12 +4,9 @@
 require('override-require')(
   (request) =>
     request.endsWith('@polkadot/wasm-crypto-asmjs') ||
-    request.endsWith('@polkadot/wasm-crypto-wasm') ||
-    request.includes('package-info.json'),
+    request.endsWith('@polkadot/wasm-crypto-wasm'),
   (request) =>
-    request.includes('package-info.json')
-      ? require('../../wasm-crypto/build/package-info.json')
-      : request.endsWith('@polkadot/wasm-crypto-asmjs')
-        ? require('../../wasm-crypto-asmjs/build/data')
-        : require('../../wasm-crypto-wasm/build/empty')
+    request.endsWith('@polkadot/wasm-crypto-asmjs')
+      ? require('../../wasm-crypto-asmjs/build/data')
+      : require('../../wasm-crypto-wasm/build/empty')
 );
