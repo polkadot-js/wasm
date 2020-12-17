e7389c2f158b7d898aeb3f9f8dd8a33d089a770d
Jaco Greeff <jacogr@gmail.com>
2020-12-17 17:37:12 +0100

    Remove non-working import.meta wrapper

--- packages/wasm-crypto/src/detectPackage.mjs
+++ packages/wasm-crypto/src/detectPackage.mjs
@@ -5,7 +5,6 @@ import { detectPackage } from '@polkadot/util';
 
 import packageInfo from './package-info.json';
 
-// eslint-disable-next-line @typescript-eslint/no-implied-eval,no-new-func
-detectPackage(packageInfo, () => new Function('import()') && import.meta.url);
+detectPackage(packageInfo, () => import.meta.url);
 
 export { packageInfo };
