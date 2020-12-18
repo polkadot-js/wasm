e7b064fb727c72111b675eb6514ff9ea37e311aa
Jaco Greeff <jacogr@gmail.com>
2020-12-18 21:36:24 +0100

    Bundle unzlibSync (no worker requirement, stop-gap)

--- packages/wasm-crypto-wasm/src/data.ts
+++ packages/wasm-crypto-wasm/src/data.ts
@@ -1,8 +1,9 @@
 // Copyright 2017-2020 @polkadot/wasm-crypto-wasm authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { unzlibSync } from 'fflate';
+// import { unzlibSync } from 'fflate';
 
 import { buffer } from './buffer';
+import { unzlibSync } from './fflate';
 
 export const wasmBytes = unzlibSync(buffer);
