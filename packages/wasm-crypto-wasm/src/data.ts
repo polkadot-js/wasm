3035446bfad3315528f94988569615425cabbf1d
Jaco Greeff <jacogr@gmail.com>
2020-12-19 11:18:17 +0100

    Pass uncompressedSize

--- packages/wasm-crypto-wasm/src/data.ts
+++ packages/wasm-crypto-wasm/src/data.ts
@@ -1,9 +1,7 @@
 // Copyright 2017-2020 @polkadot/wasm-crypto-wasm authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// import { unzlibSync } from 'fflate';
-
-import { buffer } from './buffer';
+import { buffer, sizeUncompressed } from './buffer';
 import { unzlibSync } from './fflate';
 
-export const wasmBytes = unzlibSync(buffer);
+export const wasmBytes = unzlibSync(buffer, new Uint8Array(sizeUncompressed));
