95234946015f285fbc755c1ca8831bfee792140a
Jaco Greeff <jacogr@gmail.com>
2021-03-04 17:43:57 +0100

    Adjust alias imports

--- packages/wasm-crypto/test/alias-imports.cjs
+++ packages/wasm-crypto/test/alias-imports.cjs
@@ -1,12 +1,19 @@
 // Copyright 2019-2021 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+const rootIncl = ['@polkadot/wasm-crypto-asmjs', '@polkadot/wasm-crypto-wasm'];
+const detcIncl = ['@polkadot/wasm-crypto-asmjs/packageInfo', '@polkadot/wasm-crypto-wasm/packageInfo'];
+
 require('override-require')(
   (request) =>
-    request.endsWith('@polkadot/wasm-crypto-asmjs') ||
-    request.endsWith('@polkadot/wasm-crypto-wasm'),
+    rootIncl.some((p) => request.endsWith(p)) ||
+    detcIncl.some((p) => request.endsWith(p)),
   (request) =>
-    request.endsWith('@polkadot/wasm-crypto-asmjs')
-      ? require('../../wasm-crypto-asmjs/build/data.cjs')
-      : require('../../wasm-crypto-wasm/build/empty.cjs')
+    rootIncl.some((p) => request.endsWith(p))
+      ? request.endsWith(rootIncl[0])
+        ? require('../../wasm-crypto-asmjs/build/data.cjs')
+        : require('../../wasm-crypto-wasm/build/empty.cjs')
+      : request.endsWith(detcIncl[1])
+        ? require('../../wasm-crypto-asmjs/build/packageInfo.cjs')
+        : require('../../wasm-crypto-wasm/build/packageInfo.cjs')
 );
