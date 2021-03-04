eeac266b9937b4112bc9d2dd786022a65eef2e53
Jaco Greeff <jacogr@gmail.com>
2021-03-04 17:53:29 +0100

    Adjust

--- packages/wasm-crypto/test/alias-imports.cjs
+++ packages/wasm-crypto/test/alias-imports.cjs
@@ -11,5 +11,5 @@ require('override-require')(
   (request) =>
     rootIncl.some((p) => request.endsWith(p))
       ? require(`${rootIncl.find((p) => request.endsWith(p))}/build/data.cjs`)
-      : require(`${detcIncl.find((p) => request.endsWith(p))}/build/packageInfo.cjs`)
+      : require(`${detcIncl.find((p) => request.endsWith(p)).replace('packageInfo', 'build/packageInfo.cjs')}`)
 );
