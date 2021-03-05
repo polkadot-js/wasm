11a813e98a101e1434c01ea08f43762cea79301c
Jaco Greeff <jacogr@gmail.com>
2021-03-05 22:44:41 +0100

    Apply suggestions from code review

--- packages/wasm-crypto-wasm/src/bytes.ts
+++ packages/wasm-crypto-wasm/src/bytes.ts
@@ -3,11 +3,8 @@
 
 import { bytes as _bytes, sizeCompressed as _sizeCompressed, sizeUncompressed as _sizeUncompressed } from './cjs/bytes';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const bytes = _bytes;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const sizeCompressed = _sizeCompressed;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const sizeUncompressed = _sizeUncompressed;
