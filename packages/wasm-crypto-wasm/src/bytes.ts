494884adee5d96e2fd486f1eb421b8af3a78410d
Jaco Greeff <jacogr@gmail.com>
2021-03-05 22:43:08 +0100

    Apply suggestions from code review

--- packages/wasm-crypto-wasm/src/bytes.ts
+++ packages/wasm-crypto-wasm/src/bytes.ts
@@ -1,13 +1,13 @@
 // Copyright 2019-2021 @polkadot/wasm-crypto-wasm authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { bytes as _bytes, sizeCompressed as _sizeUCompressed, sizeUncompressed as _sizeUncompressed } from './cjs/bytes';
+import { bytes as _bytes, sizeCompressed as _sizeCompressed, sizeUncompressed as _sizeUncompressed } from './cjs/bytes';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const bytes = _bytes;
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const sizeCompressed = _sizeUCompressed;
+export const sizeCompressed = _sizeCompressed;
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const sizeUncompressed = _sizeUncompressed;
