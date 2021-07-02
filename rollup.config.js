63ecb19b131b803825d1413a5c06db16b804f3ca
Jaco Greeff <jacogr@gmail.com>
2021-07-02 14:43:23 +0300

    Allow multiple bundles (consistency)

--- rollup.config.js
+++ rollup.config.js
@@ -5,29 +5,31 @@ import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  external: [
-    '@polkadot/util',
-    '@polkadot/x-randomvalues'
-  ],
-  input: 'packages/wasm-crypto/build/index.js',
-  output: {
-    file: 'build/wasm-crypto/bundle/polkadotWasmCrypto.js',
-    format: 'iife',
-    globals: {
-      '@polkadot/util': 'polkadotUtil',
-      '@polkadot/x-randomvalues': 'polkadotXRandomvalues'
+export default [
+  {
+    external: [
+      '@polkadot/util',
+      '@polkadot/x-randomvalues'
+    ],
+    input: 'packages/wasm-crypto/build/index.js',
+    output: {
+      file: 'build/wasm-crypto/bundle/polkadotWasmCrypto.js',
+      format: 'iife',
+      globals: {
+        '@polkadot/util': 'polkadotUtil',
+        '@polkadot/x-randomvalues': 'polkadotXRandomvalues'
+      },
+      name: 'polkadotWasmCrypto'
     },
-    name: 'polkadotWasmCrypto'
-  },
-  plugins: [
-    alias({
-      entries: [
-        { find: '@polkadot/wasm-crypto-asmjs', replacement: '../../wasm-crypto-asmjs/build' },
-        { find: '@polkadot/wasm-crypto-wasm', replacement: '../../wasm-crypto-wasm/build' }
-      ]
-    }),
-    nodeResolve(),
-    commonjs()
-  ]
-};
+    plugins: [
+      alias({
+        entries: [
+          { find: '@polkadot/wasm-crypto-asmjs', replacement: '../../wasm-crypto-asmjs/build' },
+          { find: '@polkadot/wasm-crypto-wasm', replacement: '../../wasm-crypto-wasm/build' }
+        ]
+      }),
+      nodeResolve(),
+      commonjs()
+    ]
+  }
+];
