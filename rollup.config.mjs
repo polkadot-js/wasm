3c3f55c3b3c3dd43756df48c5604e33efb7c94a9
Jaco Greeff <jacogr@gmail.com>
2021-07-06 08:41:52 +0300

    Use createBundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,27 @@
 // Copyright 2019-2021 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { createInput, createOutput, createPlugins } from '@polkadot/dev/config/rollup';
+import { createBundle } from '@polkadot/dev/config/rollup';
+
+const pkgs = [
+  '@polkadot/wasm-crypto'
+];
 
 const external = [
+  ...pkgs,
   '@polkadot/util',
   '@polkadot/x-randomvalues'
 ];
 
-export default [
-  {
-    external,
-    input: createInput('wasm-crypto'),
-    output: createOutput('wasm-crypto', external),
-    plugins: createPlugins(
-      ['wasm-crypto-asmjs', 'wasm-crypto-wasm'].map((p) => ({
-        find: `@polkadot/${p}`,
-        replacement: `../../${p}/build`
-      }))
-    )
+const overrides = {
+  '@polkadot/wasm-crypto': {
+    entries: ['wasm-crypto-asmjs', 'wasm-crypto-wasm'].map((p) => ({
+      find: `@polkadot/${p}`,
+      replacement: `../../${p}/build`
+    }))
   }
-];
+};
+
+export default pkgs.map((pkg) =>
+  createBundle({ external, pkg, ...(overrides[pkg] || {}) })
+);
