06bb7ae0906d2a3de46b426774d27d251c1c36a1
Jaco <jacogr@gmail.com>
2021-09-01 19:43:16 +0300

    Jest config cleanup (#213)
    * Jest config cleanup
    
    * Bump

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,11 +9,5 @@ module.exports = Object.assign({}, config, {
     '@polkadot/wasm-crypto-wasm(.*)$': '<rootDir>/packages/wasm-crypto-wasm/build/$1',
     // eslint-disable-next-line sort-keys
     '@polkadot/wasm-crypto(.*)$': '<rootDir>/packages/wasm-crypto/src/$1'
-  },
-  modulePathIgnorePatterns: [
-    '<rootDir>/packages/wasm-crypto-asmjs/build',
-    '<rootDir>/packages/wasm-crypto-wasm/build',
-    '<rootDir>/packages/wasm-crypto/build'
-  ],
-  transformIgnorePatterns: ['/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)']
+  }
 });
