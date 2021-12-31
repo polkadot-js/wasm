3454f0685c3c0bd4284d711cb891647d32cbc91b
Jaco <jacogr@gmail.com>
2021-12-31 16:23:05 +0100

    Introduce detectOther (#262)

--- packages/wasm-crypto/src/detectPackage.ts
+++ packages/wasm-crypto/src/detectPackage.ts
@@ -2,10 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { detectPackage } from '@polkadot/util';
-import { packageInfo as asmInfo } from '@polkadot/wasm-crypto-asmjs/packageInfo';
-import { packageInfo as wasmInfo } from '@polkadot/wasm-crypto-wasm/packageInfo';
 
 import __dirname from './cjs/dirname';
+import others from './detectOther';
 import { packageInfo } from './packageInfo';
 
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [asmInfo, wasmInfo]);
+detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, others);
