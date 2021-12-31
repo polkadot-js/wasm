b623d89a09e8096670dd29a58b075cf6dc681f2e
Jaco <jacogr@gmail.com>
2021-12-31 11:10:49 +0200

    Mock for __dirname

--- packages/wasm-crypto/src/detectPackage.ts
+++ packages/wasm-crypto/src/detectPackage.ts
@@ -5,6 +5,7 @@ import { detectPackage } from '@polkadot/util';
 import { packageInfo as asmInfo } from '@polkadot/wasm-crypto-asmjs/packageInfo';
 import { packageInfo as wasmInfo } from '@polkadot/wasm-crypto-wasm/packageInfo';
 
+import __dirname from './cjs/dirname';
 import { packageInfo } from './packageInfo';
 
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [asmInfo, wasmInfo]);
+detectPackage(packageInfo, typeof __dirname === 'string' && __dirname, [asmInfo, wasmInfo]);
