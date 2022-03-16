be5778bf8d67bca09bfd5502bcd09b1cea531730
Jaco <jacogr@gmail.com>
2022-03-16 09:15:18 +0200

    Move generated output into named folder

--- packages/wasm-crypto-wasm/src/bytes.ts
+++ packages/wasm-crypto-wasm/src/bytes.ts
@@ -1,7 +1,7 @@
 // Copyright 2019-2022 @polkadot/wasm-crypto-wasm authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { bytes as _bytes, sizeCompressed as _sizeCompressed, sizeUncompressed as _sizeUncompressed } from './cjs/bytes';
+import { bytes as _bytes, sizeCompressed as _sizeCompressed, sizeUncompressed as _sizeUncompressed } from './generated/bytes';
 
 export const bytes = _bytes;
 
