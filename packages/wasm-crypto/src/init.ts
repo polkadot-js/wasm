334f2d65e888b35dde01db0d41e7bb42939a1710
Jaco <jacogr@gmail.com>
2022-03-23 15:13:33 +0200

    Rename wbg

--- packages/wasm-crypto/src/init.ts
+++ packages/wasm-crypto/src/init.ts
@@ -6,20 +6,20 @@ import type { AsmCreator, WasmCryptoInstance } from './types';
 import { assert } from '@polkadot/util';
 
 import { __bridge } from './bridge';
-import * as imports from './imports';
+import * as wbg from './imports';
 
 async function createPromise (wasmBytes: Uint8Array | null, asmFn: AsmCreator | null): Promise<void> {
   try {
     assert(typeof WebAssembly === 'object' && typeof WebAssembly.instantiate === 'function' && wasmBytes && wasmBytes.length, 'WebAssembly is not available in your environment');
 
-    const source = await WebAssembly.instantiate(wasmBytes, { wbg: imports });
+    const source = await WebAssembly.instantiate(wasmBytes, { wbg });
 
     __bridge.wasm = (source.instance.exports as unknown as WasmCryptoInstance);
   } catch (error) {
     // if we have a valid supplied asm.js, return that
     if (asmFn) {
       __bridge.type = 'asm';
-      __bridge.wasm = asmFn(imports);
+      __bridge.wasm = asmFn(wbg);
     } else {
       console.error(`FATAL: Unable to initialize @polkadot/wasm-crypto:: ${(error as Error).message}`);
 
