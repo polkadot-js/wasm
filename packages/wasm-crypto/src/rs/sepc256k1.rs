1ea2631ab28fbd882ccf536f3e65f8afed3278b1
Jaco <jacogr@gmail.com>
2022-03-24 10:31:18 +0200

    compress/uncompress

--- packages/wasm-crypto/src/rs/sepc256k1.rs
+++ packages/wasm-crypto/src/rs/sepc256k1.rs
@@ -6,6 +6,26 @@ use secp256k1::{PublicKey, SecretKey, SECP256K1};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub fn ext_secp_pub_compress(pubkey: &[u8]) -> Vec<u8> {
+	match PublicKey::from_slice(&pubkey) {
+		Ok(p) => p
+			.serialize()
+			.to_vec(),
+		_ => panic!("Invalid pubkey provided.")
+	}
+}
+
+#[wasm_bindgen]
+pub fn ext_secp_pub_expand(pubkey: &[u8]) -> Vec<u8> {
+	match PublicKey::from_slice(&pubkey) {
+		Ok(p) => p
+			.serialize_uncompressed()
+			.to_vec(),
+		_ => panic!("Invalid pubkey provided.")
+	}
+}
+
+#[wasm_bindgen]
 pub fn ext_secp_from_seed(seed: &[u8]) -> Vec<u8> {
 	match SecretKey::from_slice(seed) {
 		Ok(s) => {
@@ -31,6 +51,24 @@ pub mod tests {
 		let seckey = hex!("4380de832af797688026ce24f85204d508243f201650c1a134929e5458b7fbae");
 		let expected = hex!("4380de832af797688026ce24f85204d508243f201650c1a134929e5458b7fbae03fd8c74f795ced92064b86191cb2772b1e3a0947740aa0a5a6e379592471fd85b");
 		let res = ext_secp_from_seed(&seckey);
+
+		assert_eq!(res[..], expected[..]);
+	}
+
+	#[test]
+	fn can_pub_compress_full() {
+		let pubkey = hex!("04b9dc646dd71118e5f7fda681ad9eca36eb3ee96f344f582fbe7b5bcdebb1307763fe926c273235fd979a134076d00fd1683cbd35868cb485d4a3a640e52184af");
+		let expected = hex!("03b9dc646dd71118e5f7fda681ad9eca36eb3ee96f344f582fbe7b5bcdebb13077");
+		let res = ext_secp_pub_compress(&pubkey);
+
+		assert_eq!(res[..], expected[..]);
+	}
+
+	#[test]
+	fn can_pub_expand_comp() {
+		let pubkey = hex!("03b9dc646dd71118e5f7fda681ad9eca36eb3ee96f344f582fbe7b5bcdebb13077");
+		let expected = hex!("04b9dc646dd71118e5f7fda681ad9eca36eb3ee96f344f582fbe7b5bcdebb1307763fe926c273235fd979a134076d00fd1683cbd35868cb485d4a3a640e52184af");
+		let res = ext_secp_pub_expand(&pubkey);
 
 		assert_eq!(res[..], expected[..]);
 	}
