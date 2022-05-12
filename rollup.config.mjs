b65569d1a423cab53423f7031d64c5678eb8e034
Jaco <jacogr@gmail.com>
2022-05-12 08:23:56 +0300

    Re-usable rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,14 +12,21 @@ const external = [
   '@polkadot/util'
 ];
 
-const overrides = {
-  '@polkadot/wasm-crypto': {
-    entries: ['bridge', 'crypto-init', 'crypto-asmjs', 'crypto-wasm', 'util'].reduce((all, p) => ({
-      ...all,
-      [`@polkadot/wasm-${p}`]: `../../wasm-${p}/build/bundle.js`
-    }), {}),
-  }
-};
+const overrides = pkgs
+  .map((n) => n.replace('@polkadot/wasm-', ''))
+  .reduce((map, n) => ({
+    ...map,
+    [`@polkadot/wasm-${n}`]: {
+      entries: [
+        'bridge',
+        'util',
+        ...['init', 'asmjs', 'wasm'].map((p) => `${n}-${p}`)
+      ].reduce((all, p) => ({
+        ...all,
+        [`@polkadot/wasm-${p}`]: `../../wasm-${p}/build/bundle.js`
+      }), {})
+    }
+  }), {});
 
 export default pkgs.map((pkg) =>
   createBundle({
