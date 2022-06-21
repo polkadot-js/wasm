a4628712f0e2bbf93e2a226c9dd09f423cec881b
Jaco <jacogr@gmail.com>
2022-06-21 20:27:52 +0300

    Adjust cjs -> deno path rewrites

--- packages/wasm-crypto-asmjs/src/mod.ts
+++ packages/wasm-crypto-asmjs/src/mod.ts
@@ -1,5 +1,4 @@
 // Copyright 2019-2022 @polkadot/wasm-crypto-asmjs authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { asmJsInit } from './deno/data.js';
-export { packageInfo } from './packageInfo';
+export * from './index';
