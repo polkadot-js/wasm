980a73dce954a65971348cfdd5ecc277bcf77e44
Jaco <jacogr@gmail.com>
2022-07-15 12:44:10 +0300

    Adjust

--- packages/wasm-crypto/test/deno.ts
+++ packages/wasm-crypto/test/deno.ts
@@ -5,6 +5,8 @@ import { createRequire } from 'https://deno.land/std@0.107.0/node/module.ts';
 
 import * as wasm from '../build-deno/mod.ts';
 
+declare const globalThis: Record<string, unknown>;
+
 // ensure we set require on globalThis, it is used is all/*.js
 const require = globalThis.require = createRequire(import.meta.url);
 const { beforeAll, tests } = require('./all/index.js');
@@ -12,7 +14,7 @@ const { beforeAll, tests } = require('./all/index.js');
 await beforeAll('wasm', wasm);
 
 Object
-  .entries<(wasm: unknown) => unknown>(tests)
+  .entries<(wasm: unknown) => void>(tests)
   .forEach(([name, test]) => {
     Deno.test(name, () => test(wasm));
   });
