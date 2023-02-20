383ca066536f73ed3bf2de6d9a30bd5e78fbdf23
Jaco <jacogr@gmail.com>
2023-02-20 12:19:47 +0200

    Swap jest test environment

--- packages/wasm-crypto/test/jest.spec.ts
+++ packages/wasm-crypto/test/jest.spec.ts
@@ -1,15 +1,15 @@
 // Copyright 2019-2023 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import * as wasm from '../build';
+import * as wasm from '../build/index.js';
 import { initRun, tests } from './all/index.js';
 
-describe('wasm-crypto', () => {
+describe('wasm-crypto', (): void => {
   beforeAll(() => initRun('wasm', wasm));
 
-  describe.each(Object.keys(tests))('%s', (name) => {
+  describe.each(Object.keys(tests))('%s', (name): void => {
     Object
       .values(tests[name])
-      .forEach((fn) => fn(wasm));
+      .forEach((fn) => (fn as (wasm: unknown) => void)(wasm));
   });
 });
