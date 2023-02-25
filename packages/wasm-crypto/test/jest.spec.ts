218d2b921748b95712e41e7cefb451a88d3f7075
Jaco <jacogr@gmail.com>
2023-02-25 10:18:40 +0200

    Remove usage of describe.each (#478)

--- packages/wasm-crypto/test/jest.spec.ts
+++ packages/wasm-crypto/test/jest.spec.ts
@@ -1,15 +1,20 @@
 // Copyright 2019-2023 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// eslint-disable-next-line spaced-comment
+/// <reference types="@polkadot/dev/node/test/node" />
+
 import * as wasm from '../build/index.js';
 import { initRun, tests } from './all/index.js';
 
 describe('wasm-crypto', (): void => {
   beforeAll(() => initRun('wasm', wasm));
 
-  describe.each(Object.keys(tests))('%s', (name): void => {
-    Object
-      .values(tests[name])
-      .forEach((fn) => (fn as (wasm: unknown) => void)(wasm));
-  });
+  for (const name of Object.keys(tests)) {
+    describe(name, (): void => {
+      Object
+        .values<(wasm: unknown) => void>(tests[name])
+        .forEach((fn) => fn(wasm));
+    });
+  }
 });
