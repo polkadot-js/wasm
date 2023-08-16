fac838273bc863b78373283321a10bc0825d4834
Jaco <jacogr@gmail.com>
2023-08-16 11:08:12 +0300

    Adjust linting

--- scripts/pack-wasm-base.mjs
+++ scripts/pack-wasm-base.mjs
@@ -1,4 +1,4 @@
-// Copyright 2019-2023 @polkadot/wasm authors & contributors
+// Copyright 2019-2023 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import { zlibSync } from 'fflate/node';
@@ -6,11 +6,12 @@ import fs from 'node:fs';
 
 import { formatNumber } from '@polkadot/util';
 
-const DIR_DENO = `./${process.env.PKG_NAME}-wasm/build-deno/deno`;
-const DIR_CJS = `./${process.env.PKG_NAME}-wasm/build/cjs`;
-const HDR = `// Copyright 2019-${new Date().getFullYear()} @polkadot/${process.env.PKG_NAME}-wasm authors & contributors\n// SPDX-License-Identifier: Apache-2.0\n\n// Generated as part of the build, do not edit\n`;
+const PKG_NAME = process.env['PKG_NAME'];
+const DIR_DENO = `./${PKG_NAME}-wasm/build-deno/deno`;
+const DIR_CJS = `./${PKG_NAME}-wasm/build/cjs`;
+const HDR = `// Copyright 2019-${new Date().getFullYear()} @polkadot/${PKG_NAME}-wasm authors & contributors\n// SPDX-License-Identifier: Apache-2.0\n\n// Generated as part of the build, do not edit\n`;
 
-const data = fs.readFileSync(`./${process.env.PKG_NAME}/build-wasm/wasm_opt.wasm`);
+const data = fs.readFileSync(`./${PKG_NAME}/build-wasm/wasm_opt.wasm`);
 const compressed = Buffer.from(zlibSync(data, { level: 9 }));
 const base64 = compressed.toString('base64');
 
