7b258424a26a54d477f181952feacf8713d8f302
Jaco <jacogr@gmail.com>
2023-08-31 15:34:48 +0300

    Collapse build:mac command into build (w/ detect)

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,19 @@ echo "*** Building JavaScript"
 echo "*** Building WASM"
 cd packages
 
-PKG_NAME=wasm-crypto ../scripts/build-wasm.sh
+unamestr=`uname`
+
+# For Mac we set extra paths for clang
+if [[ "$unamestr" == "Darwin" ]]; then
+  if [ ! -f "/opt/homebrew/opt/llvm/bin/clang" ]; then
+    echo "FATAL: Ensure you have clang installed via homebrew"
+    exit 1
+  fi
+
+  PATH="/opt/homebrew/opt/llvm/bin:$PATH" CC=/opt/homebrew/opt/llvm/bin/clang AR=/opt/homebrew/opt/llvm/bin/llvm-ar PKG_NAME=wasm-crypto ../scripts/build-wasm.sh
+else
+  PKG_NAME=wasm-crypto ../scripts/build-wasm.sh
+fi
 
 if [ -z "$WITH_DENO" ]; then
   yarn test:wasm-crypto:js
