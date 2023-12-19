e6fd1e20a56cd51cb82edd38b38dbe57c93d364f
Jaco <jacogr@gmail.com>
2023-12-19 10:42:52 +0200

    Bump dev w/ packageDetect adjustments (#557)
    * Bump dev w/ packageDetect adjustments
    
    * sideEffects

--- packages/wasm-util/src/index.ts
+++ packages/wasm-util/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2019-2023 @polkadot/wasm-util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage.js';
+import './packageDetect.js';
 
 export * from './bundle.js';
