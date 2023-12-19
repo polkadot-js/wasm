f00a9447f9ebe005712665b1ec63b9a8f11361f8
Jaco <jacogr@gmail.com>
2023-12-19 10:25:13 +0200

    Bump dev w/ packageDetect adjustments

--- packages/wasm-crypto-asmjs/src/detectOther.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2019-2023 @polkadot/wasm-crypto-asmjs authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-export default [];
--- packages/wasm-crypto-asmjs/src/index.ts
+++ packages/wasm-crypto-asmjs/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2019-2023 @polkadot/wasm-crypto-asmjs authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage.js';
+import './packageDetect.js';
 
 export * from './bundle.js';
