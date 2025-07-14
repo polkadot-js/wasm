a0f87f10ae5be3cdc4f5860794dc7bc77f2fd643
Raj <raj@blockdeep.io>
2025-07-14 11:37:33 +0530

    Use rust version which CI expects

--- scripts/rust-version.sh
+++ scripts/rust-version.sh
@@ -3,5 +3,5 @@
 # This software may be modified and distributed under the terms
 # of the Apache-2.0 license. See the LICENSE file for details.
 
-RUST_VER=stable-x86_64-unknown-linux-gnu
+RUST_VER=nightly-2021-12-02-x86_64-unknown-linux-gnu
 # RUST_VER=stable
