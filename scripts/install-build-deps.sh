540d46717bda323852af03f94a8bfe62aa37ce47
Valentin Fernandez <33705477+valentinfernandez1@users.noreply.github.com>
2025-08-11 13:33:46 -0300

    default to installed rust version (#588)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -24,6 +24,7 @@ unamestr=`uname`
 
 # toolchain with rust-src (for panic overrdides) and the right wasm32 toolchain
 rustup toolchain install $RUST_VER -c rust-src -t wasm32-unknown-unknown
+rustup default $RUST_VER
 
 if [ "$RUST_VER" != "stable" ]; then
   cargo install xargo
