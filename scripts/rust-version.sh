0d83a0a98715c1de9321a650a09a8bf134d62657
Francisco Valentim Castilho <franciscoannyon@gmail.com>
2025-11-04 12:16:24 -0300

    Fix: Switch `RUST_VER` to 1.63.0-nightly

--- scripts/rust-version.sh
+++ scripts/rust-version.sh
@@ -3,4 +3,4 @@
 # This software may be modified and distributed under the terms
 # of the Apache-2.0 license. See the LICENSE file for details.
 
-RUST_VER=nightly-2021-12-02
+RUST_VER=nightly-2022-06-24
